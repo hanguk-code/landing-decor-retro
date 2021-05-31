aa33643c55a74b46ed2172832882c5a71df51225
Orlovskyi Yuri <angelorlov@gmail.com>
2021-05-31 21:34:34 +0300

    Fix redirect old url

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -50,6 +50,13 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Throwable $exception)
     {
+
+        if ($this->isHttpException($exception)) {
+            if ($exception->getStatusCode() == 404) {
+                return redirect(env('CLIENT_URL'));
+            }
+        }
+
         return parent::render($request, $exception);
     }
 }
